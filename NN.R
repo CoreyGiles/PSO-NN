2f50c681e30f51288db9c953d4d56aacbb547f39
Corey Giles <Corey Giles>
2016-11-11 14:12:54 +0800

    Created and fixed functions. Simplified run commands

--- NN.R
+++ NN.R
@@ -26,65 +26,35 @@ GetBest<-function(net) {
     if(eval(parse(text=paste("net$Net",i,"$MSE",sep="")))<best.MSE) {
       best.MSE<-eval(parse(text=paste("net$Net",i,"$MSE",sep="")))
       best.i<-i
+      net$Best$i<-i
     }
   }
   print(c(best.MSE,best.i))
 }
 
-## NN structure
-##   first column is the weights for each input for a given neuron
-##   second column is the weights for each input for the next neuron
-##   The last row is the bias weight. It gets multiplied by the appended (1)
-
-
-## append a numeric (1) column for bias
-data.append<-cbind(data,rep(1,nrow(data)))
-nInput<-ncol(data)
-nOutputs<-ncol(data.out)
-nNeuronsL1<-6
-nNeuronsL2<-nOutputs
-
-FullNet<-list()
-FullNet$number<-200
-FullNet$Best$MSE<-1E20
-FullNet$Best$i<-0
-FullNet$Best$NNL1<-matrix(0,nrow=ncol(data.append),ncol=nNeuronsL1+1)
-FullNet$Best$NNL2<-matrix(0,nrow=ncol(FullNet$Best$NNL1),ncol=nOutputs)
-for(i in 1:FullNet$number) {
-  NNL1<-matrix(3*rnorm(ncol(data.append)*(nNeuronsL1+1)),nrow=ncol(data.append),ncol=nNeuronsL1+1)
-  NNL1[,ncol(NNL1)]<-c(rep(0,nrow(NNL1)-1),10)
-  NNL2<-matrix(3*rnorm(ncol(NNL1)*nNeuronsL2),nrow=ncol(NNL1),ncol=nNeuronsL2)
+BuildNet<-function(Input,Output,nNeurons,nParticles) {
+  data.append<-cbind(Input,rep(1,nrow(Input)))
+  nInput<-ncol(Input)
+  nOutputs<-ncol(Output)
+  nNeuronsL1<-nNeurons
+  nNeuronsL2<-nOutputs
   
-  vNNL1<-matrix(0,nrow=ncol(data.append),ncol=nNeuronsL1+1)
-  vNNL2<-matrix(0,nrow=ncol(NNL1),ncol=nNeuronsL2)
-  
-  NNL1.val<-data.append%*%NNL1
-  NNL1.val<-tanh(NNL1.val)
-  
-  NNL2.val<-NNL1.val%*%NNL2
-  
-  output<-1/(1+exp(NNL2.val))
-  
-  MSE<-sum((output-data.out)^2)
-  
-  eval(parse(text=paste("FullNet$Net",i,"$NNL1<-NNL1",sep="")))
-  eval(parse(text=paste("FullNet$Net",i,"$NNL2<-NNL2",sep="")))
-  eval(parse(text=paste("FullNet$Net",i,"$MSE<-MSE",sep="")))
-  
-  eval(parse(text=paste("FullNet$Net",i,"$pNNL1<-NNL1",sep="")))
-  eval(parse(text=paste("FullNet$Net",i,"$pNNL2<-NNL2",sep="")))
-  eval(parse(text=paste("FullNet$Net",i,"$pMSE<-MSE",sep="")))
-  
-  eval(parse(text=paste("FullNet$Net",i,"$vNNL1<-vNNL1",sep="")))
-  eval(parse(text=paste("FullNet$Net",i,"$vNNL2<-vNNL2",sep="")))
-}
-
-PrimeNet<-function(net) {
-  n<-net$number
-  for(i in 1:n) {
+  FullNet<-list()
+  FullNet$data<-data.append
+  FullNet$data.out<-Output
+  FullNet$number<-nParticles
+  FullNet$Best$MSE<-1E20
+  FullNet$Best$i<-0
+  FullNet$Best$NNL1<-matrix(0,nrow=ncol(data.append),ncol=nNeuronsL1+1)
+  FullNet$Best$NNL2<-matrix(0,nrow=ncol(FullNet$Best$NNL1),ncol=nOutputs)
+  for(i in 1:FullNet$number) {
     NNL1<-matrix(3*rnorm(ncol(data.append)*(nNeuronsL1+1)),nrow=ncol(data.append),ncol=nNeuronsL1+1)
     NNL1[,ncol(NNL1)]<-c(rep(0,nrow(NNL1)-1),10)
     NNL2<-matrix(3*rnorm(ncol(NNL1)*nNeuronsL2),nrow=ncol(NNL1),ncol=nNeuronsL2)
+    
+    vNNL1<-0.1*NNL1
+    vNNL1[nrow(vNNL1),ncol(vNNL1)]<-0
+    vNNL2<-0.1*NNL2
     
     NNL1.val<-data.append%*%NNL1
     NNL1.val<-tanh(NNL1.val)
@@ -94,6 +64,36 @@ PrimeNet<-function(net) {
     output<-1/(1+exp(NNL2.val))
     
     MSE<-sum((output-data.out)^2)
+    
+    eval(parse(text=paste("FullNet$Net",i,"$NNL1<-NNL1",sep="")))
+    eval(parse(text=paste("FullNet$Net",i,"$NNL2<-NNL2",sep="")))
+    eval(parse(text=paste("FullNet$Net",i,"$MSE<-MSE",sep="")))
+    
+    eval(parse(text=paste("FullNet$Net",i,"$pNNL1<-NNL1",sep="")))
+    eval(parse(text=paste("FullNet$Net",i,"$pNNL2<-NNL2",sep="")))
+    eval(parse(text=paste("FullNet$Net",i,"$pMSE<-MSE",sep="")))
+    
+    eval(parse(text=paste("FullNet$Net",i,"$vNNL1<-vNNL1",sep="")))
+    eval(parse(text=paste("FullNet$Net",i,"$vNNL2<-vNNL2",sep="")))
+  }
+  return(FullNet)
+}
+
+PrimeNet<-function(net) {
+  n<-net$number
+  for(i in 1:n) {
+    NNL1<-matrix(3*rnorm(nrow(net$Best$NNL1)*ncol(net$Best$NNL1)),nrow=nrow(net$Best$NNL1),ncol=ncol(net$Best$NNL1))
+    NNL1[,ncol(NNL1)]<-c(rep(0,nrow(NNL1)-1),10)
+    NNL2<-matrix(3*rnorm(nrow(net$Best$NNL2)*ncol(net$Best$NNL2)),nrow=nrow(net$Best$NNL2),ncol=ncol(net$Best$NNL2))
+    
+    NNL1.val<-net$data%*%NNL1
+    NNL1.val<-tanh(NNL1.val)
+    
+    NNL2.val<-NNL1.val%*%NNL2
+    
+    output<-1/(1+exp(NNL2.val))
+    
+    MSE<-sum((output-net$data.out)^2)
     
     eval(parse(text=paste("net$Net",i,"$NNL1<-NNL1",sep="")))
     eval(parse(text=paste("net$Net",i,"$NNL2<-NNL2",sep="")))
@@ -129,6 +129,10 @@ LearnNet<-function(net) {
     eval(parse(text=paste("NNL2<-net$Net",i,"$NNL2",sep="")))
     vNNL1<-0.7*vNNL1 + runif(1)*1.4*(bNNL1-NNL1) + runif(1)*1.4*(pNNL1-pNNL1)
     vNNL2<-0.7*vNNL2 + runif(1)*1.4*(bNNL2-NNL2) + runif(1)*1.4*(pNNL2-pNNL2)
+    vNNL1[vNNL1>2]<-2
+    vNNL1[vNNL1<(-2)]<-(-2)
+    vNNL2[vNNL2>10]<-10
+    vNNL2[vNNL2<(-10)]<-(-10)
     NNL1<-NNL1+vNNL1
     NNL2<-NNL2+vNNL2
     eval(parse(text=paste("net$Net",i,"$vNNL1<-vNNL1",sep="")))
@@ -136,7 +140,7 @@ LearnNet<-function(net) {
     eval(parse(text=paste("net$Net",i,"$NNL1<-NNL1",sep="")))
     eval(parse(text=paste("net$Net",i,"$NNL2<-NNL2",sep="")))
 
-    MSE<-EvalNet(eval(parse(text=paste("net$Net",i,sep=""))))
+    MSE<-EvalNet(net,i)
     eval(parse(text=paste("net$Net",i,"$MSE<-MSE",sep="")))
     
     if(MSE<eval(parse(text=paste("net$Net",i,"$pMSE",sep="")))) {
@@ -153,11 +157,11 @@ LearnNet<-function(net) {
   return(net)
 }
 
-EvalNet<-function(net) {
-  NNL1.val<-data.append%*%net$NNL1
+EvalNet<-function(net,selected) {
+  NNL1.val<-net$data%*%eval(parse(text=paste("net$Net",selected,"$NNL1",sep="")))
   NNL1.val<-tanh(NNL1.val)
 
-  NNL2.val<-NNL1.val%*%net$NNL2
+  NNL2.val<-NNL1.val%*%eval(parse(text=paste("net$Net",selected,"$NNL2",sep="")))
 
   output<-1/(1+exp(NNL2.val))
 
@@ -178,7 +182,7 @@ NetPerf<-function(net,selected=NULL) {
   }
 }
 
-NetPredict<-function(net,data.in,data.out) {
+NetPredict<-function(net,data.in) {
   data.append<-cbind(data.in,rep(1,nrow(data.in)))
   NNL1.val<-data.append%*%net$NNL1
   NNL1.val<-tanh(NNL1.val)
@@ -190,25 +194,39 @@ NetPredict<-function(net,data.in,data.out) {
   return(output)
 }
 
-GetBest(FullNet)
-for(i in 1:50) {
-  FullNet<-PrimeNet(FullNet)
-}
 
-for(i in 1:100){
+
+##############################################################################
+FullNet<-BuildNet(data,data.out,4,100)
+GetBest(FullNet)
+hist(NetPerf(FullNet),breaks=50)
+for(i in 1:50) {
   FullNet<-LearnNet(FullNet)
 }
 GetBest(FullNet)
 hist(NetPerf(FullNet),breaks=50)
 
-Predicted<-NetPredict(FullNet$Net151,In.Data[Test,],Out.Data[Test,])
+Predicted<-NetPredict(FullNet$Net50,In.Data[Test,])
+round(Predicted,1)-Out.Data[Test,]
+
+for(i in 1:50) {
+  FullNet<-PrimeNet(FullNet)
+}
+
+for(i in 1:10){
+  FullNet<-LearnNet(FullNet)
+}
+GetBest(FullNet)
+hist(NetPerf(FullNet),breaks=50)
+
+Predicted<-NetPredict(FullNet$Net154,In.Data[Test,])
 round(Predicted,1)-Out.Data[Test,]
 
 
 GetBest(FullNet)
-for(n in 1:5) {
+for(n in 1:3) {
   FullNet<-PrimeNet(FullNet)
-  for(i in 1:30){
+  for(i in 1:50){
     FullNet<-LearnNet(FullNet)
   }
 }
